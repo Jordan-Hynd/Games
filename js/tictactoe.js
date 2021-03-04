2d71dc15957e365f3b8496f354e7aab7c0a955a9
Jordan-Hynd <78391991+Jordan-Hynd@users.noreply.github.com>
2021-03-03 20:18:37 -0800

    complete
    added new png and mp3

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -5,9 +5,9 @@ function placeXorO (squareNumber) {
     if (!selectedSquares.some(element => element.includes(squareNumber))) {
         let select = document.getElementById(squareNumber);
         if (activePlayer === 'X') {
-            select.style.backgroundImage = 'url("images/x.png")';
+            select.style.backgroundImage = 'url("images/OIP.png")';
         } else {
-            select.style.backgroundImage = 'url("images/o.png")';
+            select.style.backgroundImage = 'url("images/OIP2.png")';
         }
 
         selectedSquares.push(squareNumber + activePlayer);
@@ -20,7 +20,7 @@ function placeXorO (squareNumber) {
             activePlayer = 'X'
         }
 
-        Audio("./media/place.mp3");
+        audio('./media/Laser.mp3');
 
         if (activePlayer === 'O') {
             disableClick();
@@ -34,7 +34,7 @@ function placeXorO (squareNumber) {
 
         let pickASquare;
         while(!success){
-            pickASquare = String(math.floor(math.random() * 9));
+            pickASquare = String(Math.floor(Math.random() * 9));
 
         if (placeXorO(pickASquare)) {
             placeXorO(pickASquare);
@@ -44,7 +44,7 @@ function placeXorO (squareNumber) {
     }
 }
 
-//win conditions
+//win conditions.
 
 function checkWinConditions() { 
     if (arrayIncludes('0X', '1X', '2X')) { drawWinLine(50, 100, 558, 100); }
@@ -71,7 +71,7 @@ function checkWinConditions() {
 
     else if (arrayIncludes('0O', '3O', '6O')) { drawWinLine(100, 50, 100, 558); }
 
-    else if (arrayIncludes('1O', '4O', '7O')) { drawWinLine(304, 50, 304, 588); }
+    else if (arrayIncludes('1O', '4O', '7O')) { drawWinLine(304, 50, 304, 558); }
 
     else if (arrayIncludes('2O', '5O', '8O')) { drawWinLine(508, 50, 508, 558); }
 
@@ -80,7 +80,7 @@ function checkWinConditions() {
     else if (arrayIncludes('0O', '4O', '8O')) { drawWinLine(100, 100, 520, 520); }
 
     else if (selectedSquares.length >= 9) {
-        Audio('./media/tie.mp3');
+        audio('./media/Audience.mp3');
         setTimeout(function() { resetGame(); }, 1000);
     }
 
@@ -97,11 +97,64 @@ function checkWinConditions() {
 
 //wait for computers turn
 function disableClick() {
-    body.style.poointerEvents = 'none';
-    setTimeout(function() {body.style.poointerEvents = 'auto';}, 1000);
+    body.style.pointerEvents = 'none';
+    setTimeout(function() {body.style.pointerEvents = 'auto';}, 1000);
 }
 
 function audio(audioURL) {
     let audio = new Audio(audioURL);
     audio.play();
+}
+
+//win line
+
+function drawWinLine(coordX1, coordY1, coordX2, coordY2) {
+    const canvas = document.getElementById('win-lines')
+    const c = canvas.getContext('2d');
+    let x1 = coordX1,
+        y1 = coordY1,
+        x2 = coordX2,
+        y2 = coordY2,
+        x = x1,
+        y = y1;
+
+    function animateLineDrawing() {
+        const animationLoop = requestAnimationFrame(animateLineDrawing);
+        c.clearRect(0, 0, 608, 608)
+        c.beginPath();
+        c.moveTo(x1, y1)
+        c.lineTo(x, y)
+        c.lineWidth = 10;
+        c.strokeStyle = 'rgba(70, 255, 33, .8)';
+        c.stroke();
+        if (x1 <= x2 && y1 <= y2) {
+            if (x < x2) {x += 10; }
+            if (y < y2) {y += 10; }
+            if (x >= x2 && y >= y2) { cancelAnimationFrame(animationLoop); }
+        }
+
+        if (x1 <= x2 && y1 >= y2) {
+            if (x < x2) {x =+ 10; }
+            if (y > y2) {y -+10; }
+            if (x>= x2 && y <= y2) { cancelAnimationFrame(animationLoop); }
+        }
+    }
+
+    function clear() {
+        const animationLoop = requestAnimationFrame (clear);
+        c.clearRect(0, 0, 608, 608);
+        cancelAnimationFrame(animationLoop);
+    }
+    disableClick();
+    audio('./media/UFO.mp3');
+    animateLineDrawing();
+    setTimeout(function () { clear(); resetGame(); }, 2000);
+}
+
+function resetGame() {
+    for (let i = 0; i < 9; i++) {
+        let square = document.getElementById(String(i))
+        square.style.backgroundImage = ''
+    }
+    selectedSquares = [];
 }
